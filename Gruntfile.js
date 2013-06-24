a584ffa1662a7fe0d46fc026d8bc235f81b5f397
Evoluteur <olivier@evolutility.org>
2013-06-23 22:12:02 -0700

    Added package.json and Gruntfile.js.
    Added package.json and Gruntfile.js.
    Modified .gitignore.

---
+++ Gruntfile.js
@@ -0,0 +1,59 @@
+module.exports = function (grunt) {
+
+    grunt.initConfig({
+
+        // *************************************************************************************
+        //      CONFIG OPTIONS
+        // *************************************************************************************
+
+        pkg: grunt.file.readJSON('package.json'),
+        banner :  '/*\n   <%= pkg.name %>\n   <%= pkg.copyright %>\n*/\n',
+
+        // *************************************************************************************
+        //      JSHINT options
+        // *************************************************************************************
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'package.json',
+                'js/evol.advancedSearch.js'
+            ]
+        },
+
+        // *************************************************************************************
+        //      UGLIFY options
+        // *************************************************************************************
+        uglify: {
+            options: {
+                banner: '<%= banner %>',
+                mangle: true
+            },
+            js: {
+                files: { 
+                'js/evol.advancedSearch.min.js': ['js/evol.advancedSearch.js']
+                }
+            }
+        }
+
+    });
+
+// Load the plugin that provides the tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+// Custom tasks
+
+    // *************************************************************************************
+    //      BUILD TASKS : dev prod
+    // *************************************************************************************
+    // Default task(s).
+    grunt.registerTask('default', ['dev']);
+
+    // Dev only task(s).
+    grunt.registerTask('dev', ['jshint']);
+
+    // Prod only task(s).
+    grunt.registerTask('prod', ['jshint', 'uglify']);
+
+};
+
