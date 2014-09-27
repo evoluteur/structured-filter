8af663062f0607235d9207d1def5be8eadc0f774
Olivier Giulieri <olivier.giulieri@illum.io>
2014-09-26 21:50:59 -0700

    Version 1.0.2.

--- js/structured-filter.js
+++ js/structured-filter.js
@@ -1,5 +1,5 @@
 ﻿/*!
- * structured-filter 1.0.1
+ * structured-filter 1.0.2
  *
  * Copyright (c) 2014, Olivier Giulieri
  *
@@ -69,7 +69,7 @@
 
 $.widget( 'evol.structFilter', {
 
-	version: '1.0.1',
+	version: '1.0.2',
 
 	options: {
 		fields: [],
