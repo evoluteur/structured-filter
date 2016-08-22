0076708ef047cfccfc60b83de00f7ddcde41a948
Martin Meszaros <m.meszaros@t-systems.com>
2016-08-22 09:13:48 +0100

    add compatibility with jQuery UI 1.12 might break compatibility with jQuery UI 1.11 and below

--- js/structured-filter.js
+++ js/structured-filter.js
@@ -101,15 +101,16 @@ $.widget( 'evol.structFilter', {
 		// - button submit
 		if(this.options.submitButton){
 			this._bSubmit=e.find('.evo-bSubmit').button({
-					text: bLabels
+					showLabel: bLabels
 				}).on('click', function(e){
 					that.element.trigger('submit.search');
 				});
 		}
 		// - editor button new
 		this._bNew=e.find('.evo-bNew').button({
-				text: bLabels,
-				icons: {secondary:'ui-icon-plusthick'}
+                showLabel: bLabels,
+				icon: 'ui-icon-plusthick',
+                iconPosition: 'end'
 			}).on('click', function(e){
 				if(that._step<1){
 					that._setEditorField();
@@ -119,8 +120,9 @@ $.widget( 'evol.structFilter', {
 			});
 		// - editor button add
 		this._bAdd=e.find('.evo-bAdd').button({
-				text: bLabels,
-				icons: {secondary:'ui-icon-check'}
+                showLabel: bLabels,
+				icon: 'ui-icon-check',
+                iconPosition: 'end'
 			}).on('click', function(evt){
 				var data=that._getEditorData();
 				if(that._cFilter){
@@ -132,8 +134,9 @@ $.widget( 'evol.structFilter', {
 			});
 		// - editor button cancel
 		this._bDel=e.find('.evo-bDel').button({
-				text: bLabels,
-				icons: {secondary:'ui-icon-close'}
+                showLabel: bLabels,
+				icon: 'ui-icon-close',
+                iconPosition: 'end'
 			}).on('click', function(evt){
 				that._removeEditor();
 			});
@@ -193,7 +196,7 @@ $.widget( 'evol.structFilter', {
 		});
 		this._filters=e.find('.evo-searchFilters').on('click', 'a', function(){
 			that._editFilter($(this));
-		}).on('click', 'a .ui-button-icon-secondary', function(evt){
+		}).on('click', 'a .ui-button-icon', function(evt){
 			evt.stopPropagation();
 			var filter=$(this).parent();
 			if(!filter.hasClass('ui-state-disabled')){
@@ -234,10 +237,11 @@ $.widget( 'evol.structFilter', {
 	},
 
 	addCondition: function(filter){
-		var f=$('<a href="javascript:void(0)">'+this._htmlFilter(filter)+'</a>')
+		var f=$('<a href="javascript:void(0)"><span>'+this._htmlFilter(filter)+'</span></a>')
 			.prependTo(this._filters)
 			.button({
-				icons: {secondary:'ui-icon-close'}
+				icon: 'ui-icon-close',
+                iconPosition: 'end'
 			})
 			.data('filter', filter)
 			.fadeIn();
